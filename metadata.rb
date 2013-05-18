6fc9a33e5a71cdd29b2c6c37c0311454de06fab4
bakins <brian.akins@turner.com>
2013-05-18 10:38:29 -0400

    fix metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             "gdebi"
 maintainer       "Brian Akins"
-maintainer_email "brian.akins@turner.com"
+maintainer_email "brian@akins.org"
 license          "All rights reserved"
 description      "Installs/Configures gdebi"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.0"
-
+license          "Apache 2"
+homepage         "https://github.com/bakins/cookbook-gdebi"
